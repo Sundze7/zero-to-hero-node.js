0b28186b42ec6c09e09125f460ff873cd1936cdf
Sundze <keyonmwesundze@mail.com>
2025-05-12 15:15:24 +0100

    mvc pattern

---
+++ 4-express/models/friends.model.js
@@ -0,0 +1,7 @@
+const friends = [
+  { id: 0, name: "Papa Abraham" },
+  { id: 1, name: "Jacob Isreal" },
+  { id: 2, name: "King David" },
+];
+
+module.exports = friends;
--- 4-express/server.js
+++ 4-express/server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 
-const app = express();
+const friendsController = require("./controllers/friends.controller");
+const messagesController = require("./controllers/messages.controller");
 
-const friends = [
-  { id: 0, name: "Papa Abraham" },
-  { id: 1, name: "Jacob Isreal" },
-  { id: 2, name: "King David" },
-];
+const app = express();
 
 // Middleware
 app.use((req, res, next) => {
@@ -18,37 +15,12 @@ app.use((req, res, next) => {
 
 app.use(express.json()); // express build-in middleware
 
-app.post("/friends", (req, res) => {
-  if (!req.body.name) {
-    return res.status(400).json({
-      error: "Missing friend name",
-    });
-  }
+app.post("/friends", friendsController.postFriend);
+app.get("/friends", friendsController.getAllFriends);
+app.get("/friends/:friendId", friendsController.getFriend);
 
-  const newFriend = {
-    name: req.body.name,
-    id: friends.length,
-  };
-  friends.push(newFriend);
-
-  res.json(newFriend);
-});
-
-app.get("/friends", (req, res) => {
-  res.json(friends);
-});
-app.get("/friends/:friendId", (req, res) => {
-  const friendId = Number(req.params.friendId);
-  //const friendId = +req.params.friendId => + converts this string to a number
-  const friend = friends[friendId];
-  if (friend) {
-    res.json(friend);
-  } else {
-    res.status(404).json({
-      error: "Friend doesn't exist",
-    });
-  }
-});
+app.get("/messages", messagesController.getMessages);
+app.post("/messages", messagesController.postMessage);
 
 const PORT = 3000;
 
