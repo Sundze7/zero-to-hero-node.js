132db38c9e4fe66f0974ef19e6dcac13f8d3daa6
Sundze <keyonmwesundze@mail.com>
2025-05-16 19:16:04 +0100

    nasa-project example - serverside data

--- 5-nasa-project/server/src/app.js
+++ 5-nasa-project/server/src/app.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const planetsRouter = require("./routes/planets/planets.router.js");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3002",
+  })
+);
+
 app.use(express.json()); // will parse any json from the body of incoming reqs
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
